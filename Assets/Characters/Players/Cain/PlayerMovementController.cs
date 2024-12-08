981dc11a3d6a14a5d2f32b9237f3e994ba8f369a
Mohey Karam <moheykaramdev@gmail.com>
2024-12-08 20:54:53 +0200

    Added-Cains-Sprite-topdown

--- Assets/Characters/Players/Cain/PlayerMovementController.cs
+++ Assets/Characters/Players/Cain/PlayerMovementController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
@@ -5,10 +6,12 @@ public class PlayerMovementController : MonoBehaviour
     [SerializeField] float movementSpeed = 2f;
     Vector2 _movementInput;
     Rigidbody2D _rb;
+    Animator _anim;
     // Start is called before the first frame update
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();    
+        _anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -25,7 +28,21 @@ public class PlayerMovementController : MonoBehaviour
     void Move()
     {
         _rb.velocity = _movementInput * movementSpeed;
+        ChooseAnimation();
     }
+
+    private void ChooseAnimation()
+    {
+        if (_rb.velocity.x > 0)
+            _anim.Play("right"); 
+        else if (_rb.velocity.x < 0)
+            _anim.Play("left");
+        else if (_rb.velocity.y > 0)
+            _anim.Play("up");
+        else if (_rb.velocity.y < 0)
+            _anim.Play("down");
+    }
+
     void GetKeyInput()
     {
         _movementInput.x = Input.GetAxisRaw("Horizontal");

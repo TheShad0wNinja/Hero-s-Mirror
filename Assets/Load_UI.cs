820bf4f497f44254a2e3ebbb89b993be20ef42a5
PeterEhabDev <pehab0666@gmail.com>
2024-12-15 18:50:16 +0200

    UI manager level

---
+++ Assets/Load_UI.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class Load_UI : MonoBehaviour
+{
+    public static Load_UI Instance;
+
+    public GameObject leveUIPanelPrefab;
+    private GameObject levelPanel;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+
+        Invoke("FindOrCreateLevelPanel",0.1f);
+    }
+    private void FindOrCreateLevelPanel()
+    {
+        levelPanel = GameObject.FindGameObjectWithTag("LevelPanel");
+
+        if (levelPanel == null)
+        {
+            GameObject canvas = GameObject.Find("Canvas");
+            if (canvas == null) throw new System.Exception("Canvas not found in scene.");
+
+            levelPanel = Instantiate(leveUIPanelPrefab, canvas.transform);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

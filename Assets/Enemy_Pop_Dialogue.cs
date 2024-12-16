b9bf87c4b0acaf03bec3b196554553c8760cc751
PeterEhabDev <pehab0666@gmail.com>
2024-12-16 06:46:53 +0200

    changes

--- Assets/Enemy_Pop_Dialogue.cs
+++ Assets/Enemy_Pop_Dialogue.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum CombatBackgroundTypes
+{
+    FOREST,
+    DUNGEON,
+    HOUSE
+}
 public class Enemy_Pop_Dialogue : MonoBehaviour
 {
     bool oneTime = false;
@@ -10,55 +16,63 @@ public class Enemy_Pop_Dialogue : MonoBehaviour
     [SerializeField] ParticleSystem deathParticles;
     bool switchConvo = false;
     public int xp, gold;
+    public List<UnitSO> enemies;
+    public CombatBackgroundTypes combatBackgroundType = CombatBackgroundTypes.FOREST;
+    bool hasTriggered = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!oneTime) 
+        if (!oneTime && collision.CompareTag("Player"))
         {
             oneTime = true;
-           Dialogue_Manager.Instance.InitializeConversation(conversation1,gameObject);
+            Dialogue_Manager.Instance.InitializeConversation(conversation1, gameObject);
         }
+    }
+    void initiateCombat()
+    {
+        CombatEnemyManager.Instance.AssignEnemies(enemies);
+        CombatEnemyManager.Instance.combatBackgroundType = combatBackgroundType;
+        Scene_Manager.Instance.ChangeSceneAdditiveRemoveLight("Combat");
     }
     private void Start()
     {
         Dialogue_Manager.Instance.onDialogueEnd += dialougueEnded;
     }
-    void dialougueEnded(GameObject user) 
+    void dialougueEnded(GameObject user)
     {
-        // start combat
-        print("dialogue ended");
-        print("is user : " + (gameObject == user));
-        if (gameObject == user) 
+        if (gameObject == user)
         {
-            if (!switchConvo) CombatWon();
+            if (!hasTriggered)
+            {
+                initiateCombat();
+                hasTriggered = true;
+            }
             else
             {
-                Death();
-                print("Death Event");
-            } 
+                if (!switchConvo) CombatWon();
+                else
+                {
+                    Death();
+                }
+            }
         }
-
-    }
-    void CombatWon() 
-    {
-        Dialogue_Manager.Instance.InitializeConversation(conversation2, gameObject);
-        switchConvo = true;
-        print("combatwon" + switchConvo);
-    }
-    void Death() 
-    {
-        print("die");
-        Instantiate(deathParticles, transform.position, Quaternion.identity);
-        Reward();
-        Dialogue_Manager.Instance.onDialogueEnd -= dialougueEnded;
-        Destroy(gameObject);
-    }
-    void Reward() 
-    {
-        UI_Behaviour_Manager.Instance.gold += gold;
-        Load_UI.Instance.levelPanel.GetComponent<Level_UI_Manager>().UpdateUI();
-        foreach (var element in UI_Behaviour_Manager.Instance.teamAssembleCharacters) 
+        void CombatWon()
         {
-            element.xp += xp;
+            Dialogue_Manager.Instance.InitializeConversation(conversation2, gameObject);
+            switchConvo = true;
+        }
+        void Death()
+        {
+            print("die");
+            Instantiate(deathParticles, transform.position, Quaternion.identity);
+            Reward();
+            Dialogue_Manager.Instance.onDialogueEnd -= dialougueEnded;
+            Destroy(gameObject);
+        }
+        void Reward()
+        {
+            UI_Behaviour_Manager.Instance.gold += gold;
+            Load_UI.Instance.levelPanel.GetComponent<Level_UI_Manager>().UpdateUI();
         }
     }
 }
